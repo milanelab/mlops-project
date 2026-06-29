684ef99694a896b624d6cab066b554dd3bb7d4f4
Uros <uros@example.com>
2026-06-29 07:14:49 +0000

    final MLOps + CML setup

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,8 +1,9 @@
 import os
 import json
-import pandas as pd
 import joblib
-from sklearn.metrics import accuracy_score, classification_report
+import pandas as pd
+from sklearn.metrics import accuracy_score, classification_report, RocCurveDisplay
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("data/processed/data.csv")
 
@@ -13,19 +14,21 @@ model = joblib.load("models/model.pkl")
 
 preds = model.predict(X)
 
+# Metrics
 accuracy = accuracy_score(y, preds)
 report = classification_report(y, preds, output_dict=True)
 
-print("Accuracy:", accuracy)
-
 os.makedirs("metrics", exist_ok=True)
 
-metrics = {
-    "accuracy": accuracy,
-    "classification_report": report
-}
-
 with open("metrics/metrics.json", "w") as f:
-    json.dump(metrics, f, indent=4)
+    json.dump({
+        "accuracy": accuracy,
+        "report": report
+    }, f, indent=4)
+
+# ROC curve
+plt.figure()
+RocCurveDisplay.from_estimator(model, X, y)
+plt.savefig("metrics/roc.png")
 
 print("Evaluation done")
